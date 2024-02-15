51a88163ab575c85e545479a118611ab26fab32d
vv1075 <vv1075@usnh.edu>
2024-02-15 02:37:09 -0500

    Implement cheese detail page

--- everycheese/cheeses/urls.py
+++ everycheese/cheeses/urls.py
@@ -8,6 +8,12 @@ urlpatterns = [
         route='',
         view=views.CheeseListView.as_view(),
         name='list'
- ),
+    ),
+     
+     path(
+        route='<slug:slug>/',
+        view=views.CheeseDetailView.as_view(),
+        name='detail'
+    ),
      
 ]
